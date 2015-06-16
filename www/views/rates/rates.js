dc6308a2afac729460a38a606996988279a38e76
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-06-16 10:33:49 -0500

    Initial

---
+++ www/views/rates/rates.js
@@ -0,0 +1,31 @@
+angular.module('App')
+.controller('RatesController', function ($scope,$ionicPopover, $http, Currencies) {
+
+  $scope.currencies = Currencies;
+
+  $ionicPopover.fromTemplateUrl('views/rates/help-popover.html', {
+    scope: $scope,
+  }).then(function (popover) {
+    $scope.popover = popover;
+  });
+  $scope.openHelp = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+
+  $scope.load = function () {
+    $http.get('https://api.bitcoinaverage.com/ticker/all').success(function (tickers) {
+      angular.forEach($scope.currencies, function (currency) {
+        currency.ticker = tickers[currency.code];
+        currency.ticker.timestamp = new Date(currency.ticker.timestamp);
+      });
+    }).finally(function(){
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+
+  };
+
+  $scope.load();
+});
